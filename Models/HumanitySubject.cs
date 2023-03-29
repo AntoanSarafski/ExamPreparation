1ea1edf0c8846d49f55f219ed461265a3277ada8
antoansarafski@protonmail.com <antoansarafski@protonmail.com>
2023-03-29 13:15:42 +0300

    Commit with EconomicalSubject.

---
+++ Models/HumanitySubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityCompetition.Models
+{
+    public class HumanitySubject : Subject
+    {
+        const double _subjectRate = 1.15;
+        public HumanitySubject(int subjectId, string subjectName)
+            : base(subjectId, subjectName, _subjectRate)
+        {
+        }
+    }
+}
