78b3481398b6a7f12cf072771637dec5a46b3a0c
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-29 18:01:59 +0300

    Commit with StudentRepository.

---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    public class StudentRepository : IRepository<IStudent>
+    {
+
+        public SubjectRepository()
+        {
+            models = new List<ISubject>();
+        }
+        private List<ISubject> models;
+
+        public IReadOnlyCollection<ISubject> Models
+        {
+            get { return models.AsReadOnly(); } //Delete setter for better encapsulation!
+        }
+
+        public void AddModel(ISubject model)
+        {
+            models.Add(model);
+        }
+
+        public ISubject FindById(int id)
+        {
+            return models.FirstOrDefault(m => m.Id == id);
+        }
+
+        public ISubject FindByName(string name)
+        {
+            return models.FirstOrDefault(n => n.Name == name);
+        }
+    }
+}
