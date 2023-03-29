b1e3a28263581ed6d1700f08a780236aae5021eb
antoansarafski@protonmail.com <antoansarafski@protonmail.com>
2023-03-29 13:14:37 +0300

    Commit with EconomicalSubject class.

---
+++ Models/EconomicalSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityCompetition.Models
+{
+    public class EconomicalSubject : Subject
+    {
+        const double _subjectRate = 1.0;
+        public EconomicalSubject(int subjectId, string subjectName) 
+            : base(subjectId, subjectName, _subjectRate)
+        {
+        }
+    }
+}
